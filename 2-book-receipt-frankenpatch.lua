2eaf06bb157ee57f569ee01c6829afab533c0268
angelsangita <147020265+angelsangita@users.noreply.github.com>
2025-11-05 11:06:22 -0700

    Update 2-book-receipt-frankenpatch.lua

--- 2-book-receipt-frankenpatch.lua
+++ 2-book-receipt-frankenpatch.lua
@@ -35,44 +35,44 @@ local quicklookbox = InputContainer:extend{
 
 function quicklookbox:init()
 
-	-- book info
-	local book_title = ""
+    -- book info
+    local book_title = ""
     local book_author = ""
     if self.ui.doc_props then
         book_title = self.ui.doc_props.display_title or ""
         book_author = self.ui.doc_props.authors or ""
-        if book_author:find("\n") then -- Show first author if multiple authors
+        if book_author:find("\n") then
             book_author =  T(_("%1 et al."), util.splitToArray(book_author, "\n")[1] .. ",")
         end
     end
-	
+    
     -- page count and book percentage
-	
-    local book_pageno = self.state.page or 1 -- Current page
+    
+    local book_pageno = self.state.page or 1
     local book_pages_total = self.ui.doc_settings.data.doc_pages or 1
     local book_pages_left  = book_pages_total - book_pageno
-    local book_percentage = (book_pageno / book_pages_total) * 100 -- Format like %.1f in header_string below
-	
+    local book_percentage = (book_pageno / book_pages_total) * 100
+    
     -- chapter Info
-	
+    
     local chapter_title = ""
     local chapter_pages_total = 0
     local chapter_pages_left = 0
     local chapter_chapter_pages_done = 0
     if self.ui.toc then
-        chapter_title = self.ui.toc:getTocTitleByPage(book_pageno) or "" -- Chapter name
+        chapter_title = self.ui.toc:getTocTitleByPage(book_pageno) or ""
         chapter_pages_total = self.ui.toc:getChapterPageCount(book_pageno) or book_pages_total
         chapter_pages_left = self.ui.toc:getChapterPagesLeft(book_pageno) or self.ui.document:getTotalPagesLeft(book_pageno)
         chapter_pages_done = self.ui.toc:getChapterPagesDone(book_pageno) or 0
     end
-    chapter_pages_done = chapter_pages_done + 1 -- This +1 is to include the page you're looking at
-	
+    chapter_pages_done = chapter_pages_done + 1
+    
     -- clock:
-	
+    
     local current_time = datetime.secondsToHour(os.time(), G_reader_settings:isTrue("twelve_hour_clock")) or ""
-	
+    
     -- battery:
-	
+    
     local battery = ""
     if Device:hasBattery() then
         local power_dev = Device:getPowerDevice()
@@ -81,199 +81,196 @@ function quicklookbox:init()
         local batt_prefix = power_dev:getBatterySymbol(power_dev:isCharged(), is_charging, batt_lvl) or ""
         battery = batt_prefix .. batt_lvl .. "%"
     end
-    	
-		
-	--==================== widget design ====================--		
-	
-		
-	local widget_width = Screen:getWidth() / 2	
-	local db_font_color = Blitbuffer.COLOR_BLACK
-	local db_font_color_lighter = Blitbuffer.COLOR_GRAY_3
-	local db_font_color_lightest = Blitbuffer.COLOR_GRAY_9
-	local db_font_face = "NotoSans-Regular.ttf"
-	local db_font_face_italics = "NotoSans-Italic.ttf"
-	local db_font_size_big = 20
-	local db_font_size_mid = 15
-	local db_font_size_small = 13
-	local db_padding = 15 -- padding b/w framecontainer and widgets
-	local db_padding_internal = 5 -- vertical padding between widgets
-	
-	function databox(db_typename, db_itemname, db_pagedone, db_pagetotal, show_title) -- one databox each for book and chapter
-	
-		local boxtitle = nil
-		if show_title then
-			boxtitle = TextWidget:new{
-			  text = db_typename,
-			  face = Font:getFace("cfont", db_font_size_big),
-			  bold = true,
-			  fgcolor = db_font_color,
-			  padding = 0,
-			}
-		end
-		
-		local book_or_chapter_name = TextBoxWidget:new{
-		  face = Font:getFace(db_font_face, db_font_size_mid),
-		  text = db_itemname,
-		  width = widget_width,
-		  fgcolor = db_font_color,
-		}
-		
-		local progressbarwidth = widget_width
-		local progress_bar = ProgressWidget:new{
-		  width = progressbarwidth,
-		  height = Screen:scaleBySize(2),
-		  percentage = db_pagedone/db_pagetotal,
-		  margin_v = 0,
-		  margin_h = 0,
-		  radius = 20,
-		  bordersize = 0,
-		  bgcolor = db_font_color_lightest,
-		  fillcolor = db_font_color,
-		}
-		
-		local page_progress = TextWidget:new {
+        
+        
+    --==================== widget design ====================--		
+    
+        
+    local widget_width = Screen:getWidth() / 2.3  
+    local db_font_color = Blitbuffer.COLOR_BLACK
+    local db_font_color_lighter = Blitbuffer.COLOR_GRAY_3
+    local db_font_color_lightest = Blitbuffer.COLOR_GRAY_9
+    local db_font_face = "NotoSans-Regular.ttf"
+    local db_font_face_italics = "NotoSans-Italic.ttf"
+    local db_font_size_big = 18  
+    local db_font_size_mid = 15 
+    local db_font_size_small = 12
+    local db_padding = 12
+    local db_padding_internal = 5 
+    
+    function databox(db_typename, db_itemname, db_pagedone, db_pagetotal, show_title)
+    
+        local boxtitle = nil
+        if show_title then
+            boxtitle = TextWidget:new{
+              text = db_typename,
+              face = Font:getFace("cfont", db_font_size_big),
+              bold = true,
+              fgcolor = db_font_color,
+              padding = 0,
+            }
+        end
+        
+        local book_or_chapter_name = TextBoxWidget:new{
+          face = Font:getFace(db_font_face, db_font_size_mid),
+          text = db_itemname,
+          width = widget_width,
+          fgcolor = db_font_color,
+        }
+        
+        local progressbarwidth = widget_width
+        local progress_bar = ProgressWidget:new{
+          width = progressbarwidth,
+          height = Screen:scaleBySize(3), 
+          percentage = db_pagedone/db_pagetotal,
+          margin_v = 0,
+          margin_h = 0,
+          radius = 20,
+          bordersize = 0,
+          bgcolor = db_font_color_lightest,
+          fillcolor = db_font_color,
+        }
+        
+        local page_progress = TextWidget:new {
           text = string.format("page %i of %i", db_pagedone, db_pagetotal),
           face = Font:getFace("cfont", db_font_size_small),
           bold = false,
           fgcolor = db_font_color_lighter,
           padding = 0,
           align = "left"
-		}
-		
-		local percentage_display = TextWidget:new {
+        }
+        
+        local percentage_display = TextWidget:new {
           text = string.format("%i%%", db_pagedone/db_pagetotal*100 ),
           face = Font:getFace("cfont", db_font_size_small),
           bold = false,
           fgcolor = db_font_color_lighter,
           padding = 0,
           align = "right"
-		}	
-		
-		local progressmodule = VerticalGroup:new{
-		  progress_bar,
-		  HorizontalGroup:new{
-			page_progress, 
-			HorizontalSpan:new{width = progressbarwidth - page_progress:getSize().w - percentage_display:getSize().w},
-			percentage_display,
-		  },
-		}
-		
-		local box_structure = VerticalGroup:new{}
-		
-		if boxtitle then
-			box_structure[#box_structure + 1] = boxtitle
-			box_structure[#box_structure + 1] = VerticalSpan:new{ width = db_padding_internal}
-		end
-		
-		box_structure[#box_structure + 1] = book_or_chapter_name
-		box_structure[#box_structure + 1] = VerticalSpan:new{ width = db_padding_internal}
-		box_structure[#box_structure + 1] = progressmodule
+        }	
+        
+        local progressmodule = VerticalGroup:new{
+          progress_bar,
+          HorizontalGroup:new{
+            page_progress, 
+            HorizontalSpan:new{width = progressbarwidth - page_progress:getSize().w - percentage_display:getSize().w},
+            percentage_display,
+          },
+        }
+        
+        local box_structure = VerticalGroup:new{}
+        
+        if boxtitle then
+            box_structure[#box_structure + 1] = boxtitle
+            box_structure[#box_structure + 1] = VerticalSpan:new{ width = db_padding_internal}
+        end
+        
+        box_structure[#box_structure + 1] = book_or_chapter_name
+        box_structure[#box_structure + 1] = VerticalSpan:new{ width = db_padding_internal}
+        box_structure[#box_structure + 1] = progressmodule
       
     return box_structure
-	end
+    end
 
-	local batt_pct_box = TextWidget:new {
-		text = battery,
-		face = Font:getFace("cfont", db_font_size_small),
-		bold = false,
-		fgcolor = db_font_color,
-		padding = 0,
-	  }
-	
-	local glyph_clock = "⌚"	
-	local time_box = TextWidget:new {
-		text = string.format("%s%s", glyph_clock, current_time),
-		face = Font:getFace("cfont", db_font_size_small),
-		bold = false,
-		fgcolor = db_font_color,
-		padding = 0,
-	  }
-	  
-	local bottom_bar = HorizontalGroup:new{
-		batt_pct_box,
-		HorizontalSpan:new{width = (widget_width - time_box:getSize().w - batt_pct_box:getSize().w)},
-		time_box,
-	  }
+    local batt_pct_box = TextWidget:new {
+        text = battery,
+        face = Font:getFace("cfont", db_font_size_small),
+        bold = false,
+        fgcolor = db_font_color,
+        padding = 0,
+      }
+    
+    local glyph_clock = "⌚"	
+    local time_box = TextWidget:new {
+        text = string.format("%s%s", glyph_clock, current_time),
+        face = Font:getFace("cfont", db_font_size_small),
+        bold = false,
+        fgcolor = db_font_color,
+        padding = 0,
+      }
+      
+    local bottom_bar = HorizontalGroup:new{
+        batt_pct_box,
+        HorizontalSpan:new{width = (widget_width - time_box:getSize().w - batt_pct_box:getSize().w)},
+        time_box,
+      }
   
 
-	local bookboxtitle = string.format("%s - %s", book_title, book_author)
-	local bookbox = databox("Book", bookboxtitle, book_pageno, book_pages_total, false)
-	
-	-- Add "Chapter" prefix if not already present
-	local chapter_display_title = chapter_title
-	if chapter_title ~= "" and not chapter_title:lower():match("^chapter") then
-		chapter_display_title = "Chapter " .. chapter_title
-	end
-	local chapterbox = databox("Chapter", chapter_display_title, chapter_pages_done, chapter_pages_total, false)
+    local bookboxtitle = string.format("%s - %s", book_title, book_author)
+    local bookbox = databox("Book", bookboxtitle, book_pageno, book_pages_total, false)
+    
+    -- Add "Chapter" prefix if not already present
+    local chapter_display_title = chapter_title
+    if chapter_title ~= "" and not chapter_title:lower():match("^chapter") then
+        chapter_display_title = "Chapter " .. chapter_title
+    end
+    local chapterbox = databox("Chapter", chapter_display_title, chapter_pages_done, chapter_pages_total, false)
 
-	local cover_widget
-	if self.ui and self.ui.bookinfo and self.ui.document then
-		local cover_bb = self.ui.bookinfo:getCoverImage(self.ui.document)
-		if cover_bb then
-			local cover_width = cover_bb:getWidth()
-			local cover_height = cover_bb:getHeight()
-			local max_width = widget_width
-			local max_height = math.floor(Screen:getHeight() / 6) -- Shorter height for header style
-			
-			-- Scale to maintain aspect ratio, filling the width
-			local scale = max_width / cover_width
-			local scaled_w = max_width
-			local scaled_h = math.max(1, math.floor(cover_height * scale))
-			
-			-- Only scale if needed
-			if scale ~= 1 then
-				cover_bb = RenderImage:scaleBlitBuffer(cover_bb, scaled_w, scaled_h, true)
-			end
-			
-			-- Crop from top if taller than max_height
-			if scaled_h > max_height then
-				cover_bb = cover_bb:viewport(0, 0, scaled_w, max_height)
-			end
-			
-			cover_widget = CenterContainer:new{
-				dimen = Geom:new{
-					w = widget_width,
-					h = max_height,
-				},
-				ImageWidget:new{
-					image = cover_bb,
-					width = scaled_w,
-					height = math.min(scaled_h, max_height),
-				},
-			}
-		end
-	end
+    local cover_widget
+    if self.ui and self.ui.bookinfo and self.ui.document then
+        local cover_bb = self.ui.bookinfo:getCoverImage(self.ui.document)
+        if cover_bb then
+            local cover_width = cover_bb:getWidth()
+            local cover_height = cover_bb:getHeight()
+            local max_width = widget_width
+            local max_height = math.floor(Screen:getHeight() / 7)  
+            
+            local scale = max_width / cover_width
+            local scaled_w = max_width
+            local scaled_h = math.max(1, math.floor(cover_height * scale))
+            
+            if scale ~= 1 then
+                cover_bb = RenderImage:scaleBlitBuffer(cover_bb, scaled_w, scaled_h, true)
+            end
+            
+            if scaled_h > max_height then
+                cover_bb = cover_bb:viewport(0, 0, scaled_w, max_height)
+            end
+            
+            cover_widget = CenterContainer:new{
+                dimen = Geom:new{
+                    w = widget_width,
+                    h = max_height,
+                },
+                ImageWidget:new{
+                    image = cover_bb,
+                    width = scaled_w,
+                    height = math.min(scaled_h, max_height),
+                },
+            }
+        end
+    end
 
-	local content_children = {}
-	if cover_widget then
-		content_children[#content_children + 1] = cover_widget
-		content_children[#content_children + 1] = VerticalSpan:new{ width = db_padding_internal }
-	end
-	content_children[#content_children + 1] = bookbox
-	content_children[#content_children + 1] = VerticalSpan:new{ width = db_padding_internal }
-	content_children[#content_children + 1] = chapterbox
-	content_children[#content_children + 1] = VerticalSpan:new{ width = db_padding_internal }
-	content_children[#content_children + 1] = bottom_bar
+    local content_children = {}
+    if cover_widget then
+        content_children[#content_children + 1] = cover_widget
+        content_children[#content_children + 1] = VerticalSpan:new{ width = db_padding_internal }
+    end
+    content_children[#content_children + 1] = bookbox
+    content_children[#content_children + 1] = VerticalSpan:new{ width = db_padding_internal }
+    content_children[#content_children + 1] = chapterbox
+    content_children[#content_children + 1] = VerticalSpan:new{ width = db_padding_internal }
+    content_children[#content_children + 1] = bottom_bar
 
-	local final_frame = FrameContainer:new{
-		radius = 15,
-		bordersize = 2, 
-		padding_top = db_padding,
-		padding_right = db_padding,
-		padding_bottom = db_padding,
-		padding_left = db_padding, 
-		background = Blitbuffer.COLOR_WHITE, 
-		VerticalGroup:new(content_children),
-	}
+    local final_frame = FrameContainer:new{
+        radius = 15,  
+        bordersize = 3, 
+        padding_top = db_padding,
+        padding_right = db_padding,
+        padding_bottom = db_padding,
+        padding_left = db_padding, 
+        background = Blitbuffer.COLOR_WHITE, 
+        VerticalGroup:new(content_children),
+    }
 
-	self[1] = CenterContainer:new{
-		  dimen = Screen:getSize(), -- Use the whole screen size for centering
-		  final_frame,
-	}
-	
-	-- taps and keypresses
-	
-	if Device:hasKeys() then        
+    self[1] = CenterContainer:new{
+          dimen = Screen:getSize(),
+          final_frame,
+    }
+    
+    -- taps and keypresses
+    
+    if Device:hasKeys() then        
         self.key_events.AnyKeyPressed = { { Device.input.group.Any } }
     end
     if Device:isTouchDevice() then
@@ -283,7 +280,7 @@ function quicklookbox:init()
                 range = function() return self.dimen end,
             }
         }
-		self.ges_events.Tap = {
+        self.ges_events.Tap = {
             GestureRange:new{
                 ges = "tap",
                 range = function() return self.dimen end,
@@ -296,7 +293,7 @@ function quicklookbox:init()
             }
         }
     end
-	
+    
 end
 
 function quicklookbox:onTap()
@@ -305,14 +302,11 @@ end
 
 function quicklookbox:onSwipe(arg, ges_ev)
     if ges_ev.direction == "south" then
-        -- Allow easier closing with swipe up/down
         self:onClose()
     elseif ges_ev.direction == "east" or ges_ev.direction == "west" or ges_ev.direction == "north" then
-        self:onClose()-- -- no use for now
-        -- do end -- luacheck: ignore 541
-    else -- diagonal swipe
-		self:onClose()
-
+        self:onClose()
+    else
+        self:onClose()
     end
 end
 
@@ -325,13 +319,12 @@ quicklookbox.onAnyKeyPressed = quicklookbox.onClose
 
 quicklookbox.onMultiSwipe = quicklookbox.onClose
 
--- add to dispatcher
 
 Dispatcher:registerAction("quicklookbox_action", {
-							category="none", 
-							event="QuickLook", 
-							title=_("Book Receipt"), 
-							reader=true,})
+                            category="none", 
+                            event="QuickLook", 
+                            title=_("Book Receipt"), 
+                            reader=true,})
 
 function ReaderUI:onQuickLook()
     local widget = quicklookbox:new{
